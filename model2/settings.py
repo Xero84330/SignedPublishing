643ab571be0ebd62893861311c7951b9c978572f
Xero84330 <xero84330@gmail.com>
2025-10-29 00:48:55 +0530

    Update settings.py

--- model2/settings.py
+++ model2/settings.py
@@ -158,3 +158,7 @@ EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
+# Media (user-uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
