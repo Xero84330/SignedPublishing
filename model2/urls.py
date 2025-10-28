45312b7da04fdf3cf3c64228ab75c5852104d85b
Xero84330 <xero84330@gmail.com>
2025-10-28 23:34:28 +0530

    Update urls.py

--- model2/urls.py
+++ model2/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path('library/', include('library.urls')),
     path('moderator/', include('moderator.urls')),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
