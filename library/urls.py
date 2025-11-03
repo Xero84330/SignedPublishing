168d02d85d41693f4b47cd0a5fec19e1b460ca3c
Xero5825 <xero84330@gmail.com>
2025-11-03 15:28:35 +0530

    version:1.0.6

--- library/urls.py
+++ library/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', views.library_home, name='library'),
-    path('remove/<int:book_id>/', views.remove_from_collection, name='remove_from_collection'),
+    path('',login_required(views.library_home, login_url='login'), name='library'),
+    path('remove/<int:book_id>/',login_required(views.remove_from_collection, login_url='login'), name='remove_from_collection'),
 ]
